3fd6b1296c374da253f773a2380bf19a40550ba7
kwame-boakye <boakyekelvin306@gmail.com>
2025-06-23 20:05:35 -0500

    fixed indentation

---
+++ Arrays/LongestPalindromicSubstring.py
@@ -0,0 +1,30 @@
+"""
+Given a string s, return the longest palindromic substring in s.
+
+"""
+
+
+def longestPalindrome(s):
+    res = ""
+    max_len = 0
+
+    for i in range(len(s)):
+        # odd length
+        l, r = i, i
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > max_len:
+                res = s[l : r + 1]
+                max_len = r - l + 1
+            l -= 1
+            r += 1
+
+        # even length
+        l, r = i, i + 1
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            if (r - l + 1) > max_len:
+                res = s[l : r + 1]
+                max_len = r - l + 1
+            l -= 1
+            r += 1
+
+    return res
